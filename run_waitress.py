04cf9847ea6dd6a431a716bc8e99d5b21892db22
Grupo Koal <info@grupokoal.com>
2025-09-12 00:04:15 +0200

    fix: Configure waitress to use Render's PORT environment variable

--- run_waitress.py
+++ run_waitress.py
@@ -1,5 +1,7 @@
 from waitress import serve
 from app import app
+import os
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host='0.0.0.0', port=port)
